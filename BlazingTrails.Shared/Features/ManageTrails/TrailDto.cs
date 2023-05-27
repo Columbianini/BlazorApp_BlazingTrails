f212176d88ec30b534ec0225bdbc6ff3d2988e8e
Dongni An <da2899@columbia.edu>
2023-05-27 11:53:15 +0800

    initial commit, wait to finish form part

---
+++ BlazingTrails.Shared/Features/ManageTrails/TrailDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingTrails.Shared.Features.ManageTrails
+{
+    public class TrailDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string Location { get; set; } = default!;
+        public int TimeInMinutes { get; set; }
+        public int Length { get; set; }
+        public List<RouteInstruction> Route { get; set; } = new List<RouteInstruction>();
+
+        public class RouteInstruction
+        {
+            public int Stage { get;set; }
+            public String Description { get; set; }
+        }
+    }
+}
